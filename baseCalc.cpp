48f567d2aa27dc43f89e4ffaa28c8944dc13e153
Andrew <fakmaki99@mail.ru>
2020-11-17 22:38:35 +0500

    Updated build script and calc erros fixed

--- baseCalc.cpp
+++ baseCalc.cpp
@@ -1,11 +1,6 @@
 # include <iostream>
 using namespace std;
 
-int main()
-{
-   cout >> calc(1,2,'+');
-    return 0;
-}
 int calc(float num1, float num2,char operand){
     switch(operand)
     {
@@ -16,6 +11,12 @@ int calc(float num1, float num2,char operand){
 		case '-':
             return num1-num2;
         default:
-            return 0
+            return 0;
 	}
+}
+
+int main()
+{
+   cout << calc(1,2,'+');
+    return 0;
 }
